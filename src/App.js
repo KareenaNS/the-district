ee76319b839b033c56d18a47495daa3a05e63af5
KareenaNS <kareena012003@gmail.com>
2025-01-01 23:33:16 -0500

    idek anymore

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/leasing" element={<Leasing />} />
           <Route path="/booking" element={<Booking />} />
@@ -25,16 +25,7 @@ function App() {
           <Route path="/events" element={<Events />} />
           <Route path="/BlackHistoryMonth" element={<BlackHistoryMonth />} />
           <Route path="/LunarNewYear" element={<LunarNewYear />} />
-          <Route path="/StPatricksDay" element={<StPatricksDay />} /> */}
-          <Route path="/the-district" element={<Home />} />
-          <Route path="/the-district/gallery" element={<Gallery />} />
-          <Route path="/the-district/leasing" element={<Leasing />} />
-          <Route path="/the-district/booking" element={<Booking />} />
-          <Route path="/the-district/contact" element={<Contact />} />
-          <Route path="/the-district/events" element={<Events />} />
-          <Route path="/the-district/BlackHistoryMonth" element={<BlackHistoryMonth />} />
-          <Route path="/the-district/LunarNewYear" element={<LunarNewYear />} />
-          <Route path="/the-district/StPatricksDay" element={<StPatricksDay />} />
+          <Route path="/StPatricksDay" element={<StPatricksDay />} />
         </Routes>
         <Footer />
       </div>
